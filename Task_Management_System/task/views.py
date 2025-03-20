f627397cf3eaedee2866f6f0ae5efcdc758ce19c
shakib071 <shakibhasan071@gmail.com>
2025-03-20 15:11:00 +0600

    added edit delete complete task and finished the logics and design korbo na ekhon sikhci task finished

--- Task_Management_System/task/views.py
+++ Task_Management_System/task/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-
+from .models import TaskModel
 from .forms import taskForm
 # Create your views here.
 
@@ -15,3 +15,26 @@ def add_task(request):
     else:
         task_form = taskForm()
     return render(request,'add_task.html',{'form':task_form})
+
+def edit_task(request,id):
+    task = TaskModel.objects.get(pk=id)
+    task_form = taskForm(instance = task)
+
+    if request.method == "POST":
+        task_form = taskForm(request.POST,instance = task)
+        if task_form.is_valid():
+            task_form.save()
+            return redirect('homepage')
+    
+    return render(request,'add_task.html',{'form':task_form})
+
+def delete_task(request,id):
+    task = TaskModel.objects.get(pk=id)
+    task.delete()
+    return redirect('homepage')
+
+def complete_task(request,id):
+    task = TaskModel.objects.get(pk=id)
+    task.is_Completed = True
+    task.save()
+    return redirect('homepage')
