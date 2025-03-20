0cee91335ea6730b028bb216125e123682994736
shakib071 <shakibhasan071@gmail.com>
2025-03-20 08:44:39 +0600

    added add category form section

---
+++ Task_Management_System/taskCategory/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+   
+    path('add_category/', views.add_category,name='add_category'),
+]
--- Task_Management_System/taskCategory/views.py
+++ Task_Management_System/taskCategory/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import taskCategoryForm
 
 # Create your views here.
+
+def add_category(request):
+    if request.method == "POST":
+        add_category_form = taskCategoryForm(request.POST)
+        if add_category_form.is_valid():
+            add_category_form.save()
+            return redirect('homepage')
+    else:
+        add_category_form = taskCategoryForm()
+    return render(request,'add_category.html',{'form':add_category_form})
